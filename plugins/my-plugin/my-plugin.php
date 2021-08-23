94efa342f4ab95c4df0178dd7344f64860e4b3cb
mikitakafukane <arsene.miyaniii.4@gmail.com>
2021-08-23 23:16:20 +0900

    finish75

---
+++ plugins/my-plugin/my-plugin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: マイプラグイン
+Author: fukane
+*/
+
+add_action('enqueue_block_editor_assets', function() {
+  wp_enqueue_script(
+    'myeditor-script',
+    plugins_url('myeditor.js', __FILE__)
+  );
+});
